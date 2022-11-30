49eb237582163bddd10bdb551bd6c7133458c47c
Bela VanderVoort <bela.vandervoort@optimizely.com>
2022-11-30 14:22:37 -0600

    f solutions

---
+++ bela/2021Day1/DoStuff.cs
@@ -0,0 +1,57 @@
+﻿namespace Day1;
+
+public static class DoStuff
+{
+    public static int Run(string input)
+    {
+        var reader = new StringReader(input);
+        int? last = null;
+        var ups = 0;
+        var line = reader.ReadLine();
+        while (line != null)
+        {
+            var current = int.Parse(line);
+            if (current > last)
+            {
+                ups++;
+            }
+
+            last = current;
+            line = reader.ReadLine();
+        }
+
+        return ups;
+    }
+
+    public static int RunRunningTotal(string input)
+    {
+        var reader = new StringReader(input);
+        int? last = null;
+        var queue = new Queue<int>();
+        var ups = 0;
+        var line = reader.ReadLine();
+        while (line != null)
+        {
+            queue.Enqueue(int.Parse(line));
+            if (queue.Count > 3)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 3)
+            {
+                var current = queue.Sum(o => o);
+                if (current > last)
+                {
+                    ups++;
+                }
+
+                last = current;
+            }
+
+            line = reader.ReadLine();
+        }
+
+        return ups;
+    }
+}
--- bela/2021Day1/Tests.cs
+++ bela/2021Day1/Tests.cs
@@ -24,10 +24,35 @@ public class Run
     }
     
     [Test]
+    public void Testa()
+    {
+        var input = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        var result = DoStuff.RunRunningTotal(input);
+        result.Should().Be(5);
+    }
+    
+    [Test]
     public void Test2()
     {
         var input = File.ReadAllText("../../../Input.txt");
         var result = DoStuff.Run(input);
+        Console.WriteLine(result);
+    }
+    
+    [Test]
+    public void Test2a()
+    {
+        var input = File.ReadAllText("../../../Input.txt");
+        var result = DoStuff.RunRunningTotal(input);
         Console.WriteLine(result);
     }
 }
