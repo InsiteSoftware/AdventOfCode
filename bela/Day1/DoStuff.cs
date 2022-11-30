a4abd7ece127ac63672abc21c2b5490498498dda
Bela VanderVoort <bela.vandervoort@optimizely.com>
2022-11-30 11:24:27 -0600

    weeeee

---
+++ bela/Day1/DoStuff.cs
@@ -0,0 +1,25 @@
+﻿namespace Day1;
+
+public static class DoStuff
+{
+    public static int Run(string input)
+    {
+        var reader = new StringReader(input);
+        int? last = null;
+        var ups = 0;
+        var line = reader.ReadLine();
+        while (line != null)
+        {
+            var current = int.Parse(line);
+            if (current > last)
+            {
+                ups++;
+            }
+
+            last = current;
+            line = reader.ReadLine();
+        }
+
+        return ups;
+    }
+}
---
+++ bela/Day1/Tests.cs
@@ -0,0 +1,33 @@
+﻿namespace Day1;
+
+using FluentAssertions;
+using NUnit.Framework;
+
+[TestFixture]
+public class Run
+{
+    [Test]
+    public void Test()
+    {
+        var input = @"199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+        var result = DoStuff.Run(input);
+        result.Should().Be(7);
+    }
+    
+    [Test]
+    public void Test2()
+    {
+        var input = File.ReadAllText("../../../Input.txt");
+        var result = DoStuff.Run(input);
+        Console.WriteLine(result);
+    }
+}
