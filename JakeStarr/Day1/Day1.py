413518a25cfb398e8268bd1dbd7bfe15334c42cd
Jake Starr <jakestarr60@gmail.com>
2022-12-01 13:18:43 -0500

    shorten code

--- JakeStarr/Day1/Day1.py
+++ JakeStarr/Day1/Day1.py
@@ -15,23 +15,15 @@ def main():
             val = try_parse_int(line)
             if line == "\n":
                 valueList.append(total_val)
-
                 if total_val > highest_val:
                     highest_val = total_val
-
                 total_val = 0
                 continue
-
             total_val += val
-
-    total_val = 0
     print(f"Highest value: {highest_val}")
-
     valueList.sort(reverse=True)
-
     for i in range(3):
         total_val += valueList[i]
-
     print(total_val)
     
 if __name__ == "__main__":
