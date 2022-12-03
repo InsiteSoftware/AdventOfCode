f4ae84c8867fbbf2ba5b1403933abed351c782c1
Jake Starr <jakestarr60@gmail.com>
2022-12-03 12:35:08 -0500

    Start day 3 - fk cpp

---
+++ JakeStarr/2022/Day3/day3.py
@@ -0,0 +1,10 @@
+from io import open
+
+def main():
+    print(ord('A') - (ord('a') - 1))
+    #with open("input.txt", "r") as f:
+    #    for line in f:
+    #        print(line)
+
+if __name__ == "__main__":
+    main()
