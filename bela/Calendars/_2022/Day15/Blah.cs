fb5ee346da644b5e3d8f65559269e78b2e131cb9
Bela VanderVoort <bela.vandervoort@optimizely.com>
2022-12-19 15:55:27 -0600

    who needs efficient when you have multiple cores and time

---
+++ bela/Calendars/_2022/Day15/Blah.cs
@@ -0,0 +1,239 @@
+﻿namespace Solution;
+
+public class Solver
+{
+    private readonly InputParser _parser;
+
+    public Solver(InputParser parser) => _parser = parser;
+
+    public long SolveForPartOne(string input, int y)
+    {
+        var (sensors, beacons) = _parser.Parse(input);
+
+        sensors.Sort((left, right) => left.Position.X.CompareTo(right.Position.X));
+        beacons.Sort((left, right) => left.Position.X.CompareTo(right.Position.X));
+
+        var leftMostSensorRadiusEdge = sensors
+            .Select(sensor => sensor.Position.X - sensor.DistanceToBeacon)
+            .Order()
+            .First();
+        var rightMostSensorRadiusEdge = sensors
+            .Select(sensor => sensor.Position.X + sensor.DistanceToBeacon)
+            .OrderDescending()
+            .First();
+
+        var rangeStart = Math.Min(leftMostSensorRadiusEdge, beacons[0].Position.X);
+        var rangeEnd = Math.Max(rightMostSensorRadiusEdge, beacons[^1].Position.X);
+
+        Console.WriteLine(rangeStart);
+        Console.WriteLine(rangeEnd);
+
+        long invalidPositionCount = 0;
+
+        for (var x = rangeStart; x < rangeEnd; ++x)
+        {
+            var position = new Coordinate(x, y);
+            if (
+                sensors.Any(
+                    sensor =>
+                        position != sensor.ClosestBeacon.Position
+                        && sensor.DistanceToBeacon >= sensor.DistanceTo(position)
+                )
+            )
+            {
+                ++invalidPositionCount;
+            }
+        }
+
+        return invalidPositionCount;
+    }
+
+    public long SolveForPartTwo(string input, long upperLimit)
+    {
+        const int tuningFrequencyMultiplier = 4000000;
+
+        var (sensors, _) = _parser.Parse(input);
+        var edgeCoordinates = sensors
+            .Select(GetEdgeCoordinates)
+            .SelectMany(coordinate => coordinate);
+
+        foreach (var coordinate in edgeCoordinates)
+        {
+            if (
+                coordinate.X > 0
+                && coordinate.X < upperLimit
+                && coordinate.Y > 0
+                && coordinate.Y < upperLimit
+            )
+            {
+                var inSensorRange = sensors.Any(
+                    sensor => sensor.DistanceToBeacon >= sensor.DistanceTo(coordinate)
+                );
+
+                if (!inSensorRange)
+                {
+                    return coordinate.X * tuningFrequencyMultiplier + coordinate.Y;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    private static List<Coordinate> GetEdgeCoordinates(Sensor sensor)
+    {
+        var left = new Coordinate(
+            sensor.Position.X - sensor.DistanceToBeacon - 1,
+            sensor.Position.Y
+        );
+        var right = new Coordinate(
+            sensor.Position.X + sensor.DistanceToBeacon + 1,
+            sensor.Position.Y
+        );
+        var top = new Coordinate(
+            sensor.Position.X,
+            sensor.Position.Y + sensor.DistanceToBeacon + 1
+        );
+        var bottom = new Coordinate(
+            sensor.Position.X,
+            sensor.Position.Y - sensor.DistanceToBeacon - 1
+        );
+
+        var edges = new List<Coordinate>();
+
+        edges = edges
+            .Concat(GetCoordinatesBetween(left, top))
+            .Concat(GetCoordinatesBetween(bottom, right))
+            .Concat(GetCoordinatesBetween(top, right))
+            .Concat(GetCoordinatesBetween(left, bottom))
+            .ToList();
+
+        return edges;
+    }
+
+    private static IEnumerable<Coordinate> GetCoordinatesBetween(Coordinate start, Coordinate end)
+    {
+        var (left, right) = start.X <= end.X ? (start, end) : (end, start);
+        var coordinates = new List<Coordinate>();
+
+        if (left.Y <= end.Y)
+        {
+            for (var offset = 0; offset <= right.X - left.X; ++offset)
+            {
+                coordinates.Add(new Coordinate(left.X + offset, left.Y + offset));
+            }
+        }
+        else
+        {
+            for (var offset = 0; offset <= right.X - left.X; ++offset)
+            {
+                coordinates.Add(new Coordinate(left.X + offset, left.Y - offset));
+            }
+        }
+
+        return coordinates;
+    }
+}
+
+public class InputParser
+{
+    private const string SensorPrefix = "Sensor at ";
+    private const string BeaconPrefix = "closest beacon is at ";
+
+    private readonly Dictionary<string, Sensor> _sensors = new(); // Dictionary in case Part 2 introduces duplicate sensor coordinates.
+    private readonly Dictionary<string, Beacon> _beacons = new();
+
+    public (List<Sensor> Sensors, List<Beacon> Beacons) Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return (new List<Sensor>(), new List<Beacon>());
+        }
+
+        _sensors.Clear();
+        _beacons.Clear();
+
+        var lines = input.Trim().Split('\n');
+        foreach (var line in lines)
+        {
+            ParseLine(line);
+        }
+
+        return (Sensors: _sensors.Values.ToList(), Beacons: _beacons.Values.ToList());
+    }
+
+    private void ParseLine(string line)
+    {
+        var segments = line.Split(": ");
+        if (segments.Length != 2)
+        {
+            throw new FormatException(
+                $"Expected sensor and beacon coordinates separated by colon. Got: \"{line}\""
+            );
+        }
+
+        var beaconString = segments[1][BeaconPrefix.Length..];
+        var beacon = ParseBeacon(beaconString);
+
+        var sensorString = segments[0][SensorPrefix.Length..];
+        ParseSensor(sensorString, beacon);
+    }
+
+    private Beacon ParseBeacon(string key)
+    {
+        if (!_beacons.ContainsKey(key))
+        {
+            var beacon = new Beacon { Position = ParseCoordinateString(key) };
+            _beacons[key] = beacon;
+        }
+
+        return _beacons[key];
+    }
+
+    private void ParseSensor(string key, Beacon closestBeacon)
+    {
+        if (!_sensors.ContainsKey(key))
+        {
+            var sensor = new Sensor
+            {
+                Position = ParseCoordinateString(key),
+                ClosestBeacon = closestBeacon,
+            };
+            _sensors[key] = sensor;
+        }
+    }
+
+    private static Coordinate ParseCoordinateString(string coordinateString)
+    {
+        var segments = coordinateString.Split(", ");
+        if (segments.Length != 2)
+        {
+            throw new FormatException(
+                $"Expected \"x=[number], y=[number]\". Got: \"{coordinateString}\""
+            );
+        }
+
+        var x = long.Parse(segments[0]["x=".Length..]);
+        var y = long.Parse(segments[1]["y=".Length..]);
+
+        return new Coordinate(x, y);
+    }
+}
+
+public record Coordinate(long X, long Y);
+
+public class Beacon
+{
+    public Coordinate Position { get; set; } = new(0, 0);
+}
+
+public class Sensor
+{
+    public Coordinate Position { get; set; } = new(0, 0);
+    public Beacon ClosestBeacon { get; set; } = new();
+
+    public long DistanceToBeacon => DistanceTo(ClosestBeacon.Position);
+
+    public long DistanceTo(Coordinate position) =>
+        Math.Abs(Position.X - position.X) + Math.Abs(Position.Y - position.Y);
+}
---
+++ bela/Calendars/_2022/Day15/DoWork.cs
@@ -0,0 +1,169 @@
+﻿namespace Calendars._2022.Day15;
+
+using System.Numerics;
+using Solution;
+
+public static class DoWork
+{
+    public static int FirstPart(string input, int row)
+    {
+        var sensorsAndDistances = new List<SensorBeaconData>();
+        var beacons = new HashSet<Point>();
+
+        input = input
+            .Replace("Sensor at x=", "")
+            .Replace(" y=", "")
+            .Replace("x=", "")
+            .Replace(": closest beacon is at ", ",");
+
+        var smallestX = int.MaxValue;
+        var largestX = int.MinValue;
+
+        foreach (var line in input.SplitLines())
+        {
+            var inputs = line.Split(",");
+            var sensor = new Point(int.Parse(inputs[0]), int.Parse(inputs[1]));
+            var beacon = new Point(int.Parse(inputs[2]), int.Parse(inputs[3]));
+
+            var distanceToBeacon = sensor.DistanceTo(beacon);
+
+            smallestX = Math.Min(smallestX, sensor.X - distanceToBeacon);
+            largestX = Math.Max(largestX, sensor.X + distanceToBeacon);
+
+            sensorsAndDistances.Add(new(sensor, beacon, distanceToBeacon));
+
+            beacons.Add(beacon);
+        }
+
+        var notPossibles = 0;
+        for (var x = smallestX; x <= largestX; x++)
+        {
+            var y = row;
+
+            if (beacons.Contains(new Point(x, y)))
+            {
+                continue;
+            }
+
+            foreach (var (sensor, beacon, distanceToSensor) in sensorsAndDistances)
+            {
+                if (distanceToSensor >= sensor.DistanceTo(x, y))
+                {
+                    notPossibles++;
+                    break;
+                }
+            }
+        }
+
+        return notPossibles;
+    }
+
+    public static string SecondPart(string input, int max)
+    {
+        var sensorsAndDistances = new List<SensorBeaconData>();
+        var beacons = new HashSet<Point>();
+
+        input = input
+            .Replace("Sensor at x=", "")
+            .Replace(" y=", "")
+            .Replace("x=", "")
+            .Replace(": closest beacon is at ", ",");
+
+        var smallestX = int.MaxValue;
+        var largestX = int.MinValue;
+
+        foreach (var line in input.SplitLines())
+        {
+            var inputs = line.Split(",");
+            var sensor = new Point(int.Parse(inputs[0]), int.Parse(inputs[1]));
+            var beacon = new Point(int.Parse(inputs[2]), int.Parse(inputs[3]));
+
+            var distanceToBeacon = sensor.DistanceTo(beacon);
+
+            smallestX = Math.Min(smallestX, sensor.X - distanceToBeacon);
+            largestX = Math.Max(largestX, sensor.X + distanceToBeacon);
+
+            sensorsAndDistances.Add(new(sensor, beacon, distanceToBeacon));
+
+            beacons.Add(beacon);
+        }
+
+        Point? solution = null;
+
+        void FindStuff(int startingY, int increment)
+        {
+            for (var y = startingY; y <= max; y += increment)
+            {
+                for (var x = 0; x <= max; x++)
+                {
+                    if (beacons.Contains(new Point(x, y)))
+                    {
+                        continue;
+                    }
+
+                    var foundInvalidSpace = false;
+                    Point? furthestSensor = null;
+                    var furthestDistance = int.MinValue;
+                    foreach (var (sensor, beacon, distanceToSensor) in sensorsAndDistances)
+                    {
+                        var distanceTo = sensor.DistanceTo(x, y);
+                        if (distanceToSensor >= distanceTo)
+                        {
+                            if (distanceTo > furthestDistance && sensor.X > x)
+                            {
+                                furthestDistance = distanceTo;
+                                furthestSensor = sensor;
+                            }
+                            foundInvalidSpace = true;
+                        }
+                    }
+
+                    if (furthestSensor != null)
+                    {
+                        x += (furthestSensor.X - x) * 2;
+                    }
+
+                    if (!foundInvalidSpace)
+                    {
+                        solution = new Point(x, y);
+                    }
+                }
+            }
+        }
+
+        var threadCount = 16;
+        var waitHandles = new WaitHandle[threadCount];
+        for (var x = 0; x < threadCount; x++)
+        {
+            var handle = new EventWaitHandle(false, EventResetMode.ManualReset);
+            var thread = new Thread(() =>
+            {
+                FindStuff(x, threadCount);
+                handle.Set();
+            });
+            waitHandles[x] = handle;
+            thread.Start();
+        }
+
+        WaitHandle.WaitAll(waitHandles);
+
+        return (
+            BigInteger.Multiply(new BigInteger(solution!.X), new BigInteger(4000000)) + solution!.Y
+        ).ToString();
+    }
+
+    private record SensorBeaconData(Point Sensor, Point Beacon, int Distance);
+}
+
+public record Point(int X, int Y)
+{
+    public int DistanceTo(Point otherPoint)
+    {
+        return DistanceTo(otherPoint.X, otherPoint.Y);
+    }
+
+    public int DistanceTo(int x, int y)
+    {
+        return Math.Abs(X - x) + Math.Abs(Y - y);
+    }
+}
