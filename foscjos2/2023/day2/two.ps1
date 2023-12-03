dcc271b9702e88b74f51e683676a2a6bf68d79be
foscjos2 <Joshua.Foschi@episerver.com>
2023-12-02 20:55:43 -0600

    Works

--- foscjos2/2023/day2/two.ps1
+++ foscjos2/2023/day2/two.ps1
@@ -10,42 +10,49 @@ ForEach ($line in $inputFile) {
     $line = $line.Replace(',', '')
     $line = $line.Replace('Game ', '')
     $split = $line -split ";"
-    $currentNumber = -1
-    $currentColor = ''
-    $minRed = -1
-    $minGreen = -1
-    $minBlue = -1
 
-    $validGame = $true
+    $currentNumber = 0
+    $currentColor = ''
+    $minRed = 0
+    $minGreen = 0
+    $minBlue = 0
 
     ForEach ($item in $split) {
         $split2 = $item -split " "
 
         ForEach ($item2 in $split2) {
-            Write-Host $item2
-
-            If ($item -match "^\d+$") {
-                $currentNumber = $item
+            If ($item2 -match "^\d+$") {
+                $currentNumber = $item2
             } Else {
-                $currentColor = $item
+                $currentColor = $item2
             }
 
             If ($currentColor -ne '') {
                 [int]$currentNumber = [convert]::ToInt32($currentNumber, 10)
 
-                if ($currentNumber -gt $gameLimits[$currentColor]) {
-                    $validGame = $false
-                    break
+                # Write-Host $currentColor $currentNumber
+
+                if ($currentColor -eq 'red') {
+                    if ($currentNumber -gt $minRed) {
+                        $minRed = $currentNumber
+                    }
+                } ElseIf ($currentColor -eq 'green') {
+                    if ($currentNumber -gt $minGreen) {
+                        $minGreen = $currentNumber
+                    }
+                } ElseIf ($currentColor -eq 'blue') {
+                    if ($currentNumber -gt $minBlue) {
+                        $minBlue = $currentNumber
+                    }
                 }
 
                 $currentColor = ''
+                $currentNumber = 0
             }
         }
     }
 
-    if ($validGame -eq $true) {
-        $total += $currentGame
-    }
+    $total += ($minRed * $minGreen * $minBlue)
 }
 
 Write-Host "Total: $total"
